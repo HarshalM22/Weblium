77b81ce196ea73e5a34ca19cb931a1cb83dbadd4
HarshalM22 <harshalmankar61@gmail.com>
2025-05-26 01:49:54 +0530

    steps

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import { InferenceClient } from "@huggingface/inference";
 import { BASE_PROMPT, getSystemPrompt } from "./prompt/prompt";
 import { reactBasePrompt } from "./defaults/react";
 import { nodeBasePrompt } from "./defaults/node";
+import cors from 'cors'
 
 if (!process.env.HF_TOKEN) {
   console.error("HF_TOKEN not found in environment variables.");
@@ -12,6 +13,7 @@ if (!process.env.HF_TOKEN) {
 
 const client = new InferenceClient(process.env.HF_TOKEN);
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.post("/template", async (req, res) => {
@@ -21,15 +23,15 @@ app.post("/template", async (req, res) => {
     model: "meta-llama/Llama-3.1-8B-Instruct",
     messages: [
       {
-        role: "user",
-        content: `Retrun either node or react based on what do you think this project should be . only return a single word either 'node or 'react' . Do not return anything else`,
+        role: "system",
+        content: "Retrun either node or react based on what do you think this project should be . only return a single word either 'node or 'react' . Do not return anything else",
       },
       {
         role: "user",
         content: prompt,
       },
     ],
-    max_tokens: 200,
+    max_tokens: 1,
     temperature: 0,
   });
 
@@ -39,14 +41,14 @@ app.post("/template", async (req, res) => {
 
   if (answer == "react" || answer== "React") {
     res.json({
-      prompt: [BASE_PROMPT, ` Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n - package-lock.json\n`],
+      basePrompt: [BASE_PROMPT, ` Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n - package-lock.json\n`],
       uiPrompt : [reactBasePrompt]
     });
     return;
   }
   if (answer == "node" || answer == "Node") {
     res.json({
-      prompt: [` Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n - package-lock.json\n `],
+      basePrompt: [` Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n - package-lock.json\n `],
       uiPrompt : [nodeBasePrompt]
     });
     return;
@@ -59,20 +61,23 @@ app.post("/chat" ,async(req,res)=>{
   const messages = req.body.messages ;
   const response = await client.chatCompletionStream({
     model: "meta-llama/Llama-3.1-8B-Instruct",
-    messages: messages,
+    messages: [{role :"system",content:getSystemPrompt()},...messages],
     max_tokens: 8000,
-    stream:true ,
-    system : getSystemPrompt()
+    stream:true 
   });
+  let responseText ="";
   for await (const chunk of response) {
       const delta = chunk.choices?.[0]?.delta?.content;
       if (delta) {
         process.stdout.write(delta);
+        responseText += delta ;
       }
     }
 
 
-  res.json({})
+  res.json({
+    response:responseText 
+  })
 
 
 })
